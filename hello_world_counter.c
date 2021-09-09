55890ad02e943291e815eee5aeb494d081c8b7e4
AVO <avo@emlogic.no>
2021-09-09 23:31:56 +0200

    first commit
    simple hello_world

---
+++ hello_world_counter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+int main()
+{
+    printf("\n\n******** Hello, World! First program ready to ship to Github and Yocto Image ********\n\n");
+
+    int countNum = 10;
+    printf("Starting counting down from: %d \n\n", countNum);
+
+    while (countNum)
+    {
+        printf("Count %d\n", countNum);
+        countNum--;
+        Sleep(1000);
+    }
+
+    printf("Program finished...\n\n");
+}
